c838e8a38555c96db204697073a179ecdebd89a5
Jovan Josifoski <jovanjo.oh@gmail.com>
2023-11-03 19:17:02 +0100

    next js commit 1

---
+++ src/frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import AuctionCard from './AuctionCard';
+import AppPagination from '../components/AppPagination';
+import { Auction } from '@/types';
+import { getData } from '../actions/auctionActions';
+
+
+export default function Listings() {
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    getData(pageNumber).then(data => {
+      setAuctions(data.result);
+      setPageCount(data.pageCount);
+    })
+  }, [pageNumber])
+
+  if (auctions.length === 0) return <h3>Loading...</h3>
+
+  return (
+    <>
+      <div className='grid grid-cols-4 gap-6'>
+        {auctions.map((auction) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+      </div>
+      <div className='flex justify-center mt-4'>
+        <AppPagination 
+        pageChanged={setPageNumber}
+          currentPage={pageNumber} pageCount={pageCount} 
+        />
+      </div>
+    </>
+
+  )
+}

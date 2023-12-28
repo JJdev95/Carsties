e764b256e145f246834572b9a58219cfee5ade7e
Jovan Josifoski <jovanjo.oh@gmail.com>
2023-12-28 22:49:05 +0100

    End of section 10

--- src/frontend/web-app/app/actions/auctionActions.ts
+++ src/frontend/web-app/app/actions/auctionActions.ts
@@ -1,34 +1,37 @@
 'use server'
 
 import { Auction, PagedResult } from "@/types";
-import { getTokenWorkaround } from "./authActions";
+import { fetchWrapper } from "@/lib/fetchWrapper";
+import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query:string): Promise<PagedResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search${query}`);
-  
-    if (!res.ok) throw new Error('Failed to fetch data');
-  
-    return res.json();
+    return await fetchWrapper.get(`search${query}`)
   }
 
-export async function UpdateAuctionTest() {
+export async function updateAuctionTest() {
   const data = {
     mileage: Math.floor(Math.random() * 100000) + 1
   }
 
-  const token = await getTokenWorkaround();
+  return await fetchWrapper.put('auctions/bbab4d5a-8565-48b1-9450-5ac2a5c4a654', data);
 
-  const res = await fetch('http://localhost:6001/auctions/bbab4d5a-8565-48b1-9450-5ac2a5c4a654', {
-    method: 'PUT',
-    headers: {
-      'Content-type': 'application/json',
-      'Authorization': 'Bearer ' + token?.access_token
-    },
-    body: JSON.stringify(data)
-  })
-
-  if (!res.ok) return {status: res.status, message: res.statusText};
-
-  return res.statusText;
 }
   
+export async function createAuction(data: FieldValues) {
+  return await fetchWrapper.post('auctions', data);
+}
+
+export async function getDetailedViewData(id:string): Promise<Auction> {
+  return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(data: FieldValues, id:string) {
+  const res = await fetchWrapper.put(`auctions/${id}`, data);
+  revalidatePath(`/auctions/${id}`);
+  return res;
+}
+
+export async function deleteAuction(id:string) {
+  return await fetchWrapper.del(`auctions/${id}`);
+}
--- src/frontend/web-app/app/nav/Navbar.tsx
+++ src/frontend/web-app/app/nav/Navbar.tsx
@@ -10,7 +10,7 @@ export default async function Navbar() {
 
   return (
     <header className='
-    sticky top-0 z-50 flex justify-between bg-whiate p-5 item-center text-gray-800 shadow-md
+    sticky top-0 z-50 flex justify-between bg-white p-5 item-center text-gray-800 shadow-md
     '>
         <Logo />
         <Search />

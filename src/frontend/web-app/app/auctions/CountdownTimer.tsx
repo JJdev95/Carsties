e8abfad0634c984bda728c298eef5fad8332b8e9
Jovan Josifoski <jovanjo.oh@gmail.com>
2024-02-08 15:52:07 +0100

    End of section 13

--- src/frontend/web-app/app/auctions/CountdownTimer.tsx
+++ src/frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,18 +1,21 @@
 'use client'
 
+import { useBidStore } from '@/hooks/useBidStore';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown';
 
 type Props = {
-    auctionEnd:string
+    auctionEnd: string
 }
 
-const renderer = ({ days, hours, minutes, seconds, completed }: {days:number, hours:number, minutes:number, seconds:number, completed:boolean}) => {
+const renderer = ({ days, hours, minutes, seconds, completed }: { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
+
     return (
         <div className={`
-            border-2 border-white text-white py-1 px-2 rounded-lg flex justify-center ${completed? 'bg-red-600': (days ===0 && hours < 10) 
-            ? 'bg-amber-600' 
-            : 'bg-green-600' } 
+            border-2 border-white text-white py-1 px-2 rounded-lg flex justify-center ${completed ? 'bg-red-600' : (days === 0 && hours < 10)
+                ? 'bg-amber-600'
+                : 'bg-green-600'} 
         `}>
             {completed ? (
                 <span>Auction finished</span>
@@ -21,12 +24,21 @@ const renderer = ({ days, hours, minutes, seconds, completed }: {days:number, ho
             )}
         </div>
     )
-  };
+};
 
-export default function CountdownTimer({auctionEnd}: Props) {
-  return (
-    <div>
-        <Countdown date={auctionEnd} renderer={renderer}/>
-    </div>
-  )
+export default function CountdownTimer({ auctionEnd }: Props) {
+    const setOpen = useBidStore(state => state.setOpen);
+    const pathname = usePathname();
+
+    function auctionFinished() {
+        if (pathname.startsWith('/auctions/details')) {
+            setOpen(false);
+        }
+    }
+
+    return (
+        <div>
+            <Countdown date={auctionEnd} renderer={renderer} onComplete={auctionFinished} />
+        </div>
+    )
 }

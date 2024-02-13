4604913e174ad137afb6d9ceffffb635ed7e733e
Jovan Josifoski <jjosifoski@hotmail.com>
2024-02-13 20:32:41 +0100

    fix for my pc

--- src/frontend/web-app/app/actions/authActions.ts
+++ src/frontend/web-app/app/actions/authActions.ts
@@ -1,24 +1,29 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import {cookies, headers} from 'next/headers';
+import { cookies, headers } from 'next/headers';
 import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
 
 export async function getSession() {
+    //some issues on my local pc, with secret  being null, this is just a temporary fix until i figure it out.
+    console.log('auth secret is ' + authOptions.secret);
+    if (!authOptions.secret) {
+        authOptions.secret = 'secret';
+    }
+
     return await getServerSession(authOptions);
 }
 
 export async function getCurrentUser() {
-    
+
     try {
         const session = await getSession();
 
-        if(!session) return null;
+        if (!session) return null;
 
         return session.user;
 
-    } catch(error)
-    {
+    } catch (error) {
         return null;
     }
 }
@@ -33,5 +38,5 @@ export async function getTokenWorkaround() {
         )
     } as NextApiRequest;
 
-    return await getToken({req});
+    return await getToken({ req });
 }
